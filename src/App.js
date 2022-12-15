0a00ead549aeca4ea8a4e101c87add5d601b9c77
Tolkinxon <krosstony639@gmail.com>
2022-12-15 08:51:13 +0500

    somi tiny changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Clicker from './Components/Clicker'
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
 
 export default function App() {
 
+    const [isClicked, setClick] = useState(false)
+
     return (
-        <div>
-            <Clicker />
+        <div className='container'>
+            <h3>React App</h3>
+          
+            <button className='toggle'>Toggle Clicker</button>
         </div>
-    )
+  )
 }
