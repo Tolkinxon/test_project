0774a8ee159ba757195b55cb7f94f6708e3b7638
Tolkinxon <krosstony639@gmail.com>
2022-12-15 09:02:22 +0500

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-
+import Clicker from './Components/Clicker'
 
 export default function App() {
+  const [isClicked, setClick] = useState(false)
 
-    const [isClicked, setClick] = useState(false)
+  useEffect(()=>{
+    console.log('hello world');
+  },[])
 
-    return (
-        <div className='container'>
-            <h3>React App</h3>
-          
-            <button className='toggle'>Toggle Clicker</button>
-        </div>
+  return (
+    <div className="container">
+      <h3>React App</h3>
+      <button onClick={() => setClick(!isClicked)} >Toggle Clicker</button>
+      {isClicked && <Clicker />}
+    </div>
   )
 }
