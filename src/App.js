86dcbd88f3290cba6c33e9aa5626376f5493d219
Tolkinxon <krosstony639@gmail.com>
2022-12-15 07:16:23 +0500

    changed set... functions

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-export default function App () {
-
- const [count, setCount] = useState(0)
- const [second, setSecond] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0)
 
 
- function increment () {
-    setCount(count + 1)
- }
+  function increment() {
+    setCount((cou) => cou + 1)
+    setCount((coun) => coun + 1)
+    }
 
- function decrement () {
+  function decrement() {
     setCount(count - 1)
- }
+  }
 
- 
-
-
-
-    return(
-        <>
-            <h2>{count}</h2>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
-
-        </>
-    )
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </>
+  )
 }
