b59399341e56404d0ced32332afebd2e5917e6dd
Tolkinxon <krosstony639@gmail.com>
2022-12-15 07:01:02 +0500

    some buttons and <h1>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App () {
+
+ const [count, setCount] = useState(0)
+ const [second, setSecond] = useState(0)
+
+
+ function increment () {
+    setCount(count + 1)
+ }
+
+ function decrement () {
+    setCount(count - 1)
+ }
+
+ 
+
+
+
+    return(
+        <>
+            <h2>{count}</h2>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+
+        </>
+    )
 }
-
-export default App;
