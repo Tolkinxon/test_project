02befb78da540b67ed9b36bf5ff456e333fe56d5
Tolkinxon <krosstony639@gmail.com>
2022-12-29 08:05:17 +0500

    inputs

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css'
 import Clicker from './Components/Clicker'
 
 export default function App() {
-  const [isClicked, setClick] = useState(false)
+  // const [isClicked, setClick] = useState(false)
 
-useEffect(() => {console.log('did mount')
-   return () => console.log('bye bye'); 
-  },[isClicked])
+// useEffect(() => {console.log('did mount')
+//    return () => console.log('bye bye'); 
+//   },[isClicked])
 
+
+  // return (
+  //   <div className="container">
+  //     <h3>Hello world</h3>
+  //     <button onClick={() => setClick(!isClicked)} >Toggle Clicker</button>
+  //     {isClicked && <Clicker />}
+  //   </div>
+  // )
 
   return (
-    <div className="container">
-      <h3>Hello world</h3>
-      <button onClick={() => setClick(!isClicked)} >Toggle Clicker</button>
-      {isClicked && <Clicker />}
+    <div>
+      <h1>hello world</h1>
+      <input type='text' placeholder='input your name'/>
+      <button>Click</button>
     </div>
   )
 }
